e507772bbd8578090eac12dcbc5f735823b2f127
BITEP <87851407+PO3KYMAP@users.noreply.github.com>
2025-05-12 18:41:23 +0200

    в

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import os
 load_dotenv()
 
 API_TOKEN = os.getenv('BOT_TOKEN')
+print(f"Token loaded: {API_TOKEN[:10]}...")  # Для отладки
 
 bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
 dp = Dispatcher()
